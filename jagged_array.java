bb170829114f6245114e961b5179d9d8493a0c93
Kevin Francis <kevinfrancis967@gmail.com>
2024-10-02 19:28:01 +0530

    third commit

--- jagged_array.java
+++ jagged_array.java
@@ -3,19 +3,20 @@
         //array decleration and instantiation
         int jaggedArr [][] = new int[3][]; //only need to give the number of rows
 
-        jaggedArr[0] = new int[2];
-        jaggedArr[1] = new int[4];
-        jaggedArr[2] = new int[3];
+        jaggedArr[0] = new int[]{1,2};
+        jaggedArr[1] = new int[]{3,4,5,6};
+        jaggedArr[2] = new int[]{7,8,9};
 
-        jaggedArr[0][0] = 1;
-        jaggedArr[0][1] = 2;
-        jaggedArr[1][0] = 3;
-        jaggedArr[1][1] = 4;
-        jaggedArr[1][2] = 5;
-        jaggedArr[1][3] = 6;
-        jaggedArr[2][0] = 7;
-        jaggedArr[2][1] = 8;
-        jaggedArr[2][2] = 9;
+        //or
+        // jaggedArr[0][0] = 1;
+        // jaggedArr[0][1] = 2;
+        // jaggedArr[1][0] = 3;
+        // jaggedArr[1][1] = 4;
+        // jaggedArr[1][2] = 5;
+        // jaggedArr[1][3] = 6;
+        // jaggedArr[2][0] = 7;
+        // jaggedArr[2][1] = 8;
+        // jaggedArr[2][2] = 9;
         
         for(int i=0;i<jaggedArr.length; i++){
             for(int j=0;j<jaggedArr[i].length; j++){
